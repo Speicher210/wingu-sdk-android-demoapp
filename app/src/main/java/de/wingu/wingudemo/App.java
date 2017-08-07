b37e8d156d84ff328c126d965dcd6f92f551e380
Mateusz Armatys <mateusz.armatys@wingu.de>
2017-08-07 16:13:11 +0200

    Update wingu sdk to 0.3.1

--- app/src/main/java/de/wingu/wingudemo/App.java
+++ app/src/main/java/de/wingu/wingudemo/App.java
@@ -4,10 +4,11 @@ import android.app.Application;
 import de.wingu.sdk.WinguSDKBuilder;
 
 public class App extends Application {
+  private static final String WINGU_API_KEY = null; // TODO
 
   @Override
   public void onCreate() {
     super.onCreate();
-    WinguSDKBuilder.with(this).build();
+    WinguSDKBuilder.with(this, WINGU_API_KEY).build();
   }
 }
