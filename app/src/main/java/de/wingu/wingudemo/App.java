5909773275f61cfc6a5f5c149f1f6582c82a2c0f
Mateusz Armatys <mateusz.armatys@wingu.de>
2017-11-27 15:53:28 +0100

    Update wingu sdk to 0.3.5; background notifications.

--- app/src/main/java/de/wingu/wingudemo/App.java
+++ app/src/main/java/de/wingu/wingudemo/App.java
@@ -1,7 +1,15 @@
 package de.wingu.wingudemo;
 
 import android.app.Application;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+
+import java.util.Collection;
+
 import de.wingu.sdk.WinguSDKBuilder;
+import de.wingu.sdk.data.api.model.Channel;
+import de.wingu.sdk.notification.ChannelNotificationsConfig;
 
 public class App extends Application {
   private static final String WINGU_API_KEY = null; // TODO
@@ -9,6 +17,35 @@ public class App extends Application {
   @Override
   public void onCreate() {
     super.onCreate();
-    WinguSDKBuilder.with(this, WINGU_API_KEY).build();
+
+    final ChannelNotificationsConfig.ChannelNotificationCallback notificationCallback = new ChannelNotificationsConfig.ChannelNotificationCallback() {
+      @Override
+      public String contentText(Collection<Channel> collection) {
+        return collection.size() + " item(s) nearby";
+      }
+
+      @Override
+      public String contentTitle(Collection<Channel> collection) {
+        return "Nearby";
+      }
+
+      @Nullable
+      @Override
+      public PendingIntent pendingIntent(Collection<Channel> collection) {
+        if (collection.isEmpty()) {
+          return null;
+        }
+        return PendingIntent.getActivity(App.this, 0, new Intent(App.this, MainActivity.class), 0);
+      }
+    };
+
+    WinguSDKBuilder.with(this, WINGU_API_KEY)
+        .iBeaconBackgroundScan(true)
+        .channelNotificationsConfig(() ->
+            new ChannelNotificationsConfig.Builder(notificationCallback)
+                .setSmallIcon(R.drawable.ic_notification_small)
+                .setLargeIcon(R.drawable.ic_notification_large)
+                .build())
+        .build();
   }
 }
