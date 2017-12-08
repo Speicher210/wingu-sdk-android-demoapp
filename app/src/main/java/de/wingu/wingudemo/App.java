eb42328cea0ff40a4518cdf9bcf4421528b5967d
Mateusz Armatys <mateusz.armatys@wingu.de>
2017-12-08 15:18:22 +0100

    Update wingu sdk to 1.0.0

--- app/src/main/java/de/wingu/wingudemo/App.java
+++ app/src/main/java/de/wingu/wingudemo/App.java
@@ -3,13 +3,16 @@ package de.wingu.wingudemo;
 import android.app.Application;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import java.util.Collection;
+import java.util.Collections;
 
 import de.wingu.sdk.WinguSDKBuilder;
 import de.wingu.sdk.data.api.model.Channel;
 import de.wingu.sdk.notification.ChannelNotificationsConfig;
+import de.wingu.sdk.notification.WinguNotification;
 
 public class App extends Application {
   private static final String WINGU_API_KEY = null; // TODO
@@ -18,25 +21,15 @@ public class App extends Application {
   public void onCreate() {
     super.onCreate();
 
-    final ChannelNotificationsConfig.ChannelNotificationCallback notificationCallback = new ChannelNotificationsConfig.ChannelNotificationCallback() {
-      @Override
-      public String contentText(Collection<Channel> collection) {
-        return collection.size() + " item(s) nearby";
+    final ChannelNotificationsConfig.ChannelNotificationCallback notificationCallback = collection -> {
+      if (collection.isEmpty()) {
+        return Collections.emptyList();
       }
 
-      @Override
-      public String contentTitle(Collection<Channel> collection) {
-        return "Nearby";
-      }
-
-      @Nullable
-      @Override
-      public PendingIntent pendingIntent(Collection<Channel> collection) {
-        if (collection.isEmpty()) {
-          return null;
-        }
-        return PendingIntent.getActivity(App.this, 0, new Intent(App.this, MainActivity.class), 0);
-      }
+      final PendingIntent pendingIntent = PendingIntent.getActivity(App.this, 0, new Intent(App.this, MainActivity.class), 0);
+      final String title = collection.size() + " item(s) nearby";
+      final WinguNotification winguNotification = WinguNotification.createGroupNotification(collection, title, pendingIntent);
+      return Collections.singleton(winguNotification);
     };
 
     WinguSDKBuilder.with(this, WINGU_API_KEY)
