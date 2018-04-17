301e0713a83056d04e1236a3f18c849572e34c51
Mateusz Armatys <mateusz.armatys@wingu.de>
2018-04-17 13:25:55 +0200

    Update wingu sdk to 2.0.0

--- app/src/main/java/de/wingu/wingudemo/App.java
+++ app/src/main/java/de/wingu/wingudemo/App.java
@@ -3,42 +3,43 @@ package de.wingu.wingudemo;
 import android.app.Application;
 import android.app.PendingIntent;
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 
-import java.util.Collection;
 import java.util.Collections;
 
 import de.wingu.sdk.WinguSDKBuilder;
-import de.wingu.sdk.data.api.model.Channel;
 import de.wingu.sdk.notification.ChannelNotificationsConfig;
 import de.wingu.sdk.notification.WinguNotification;
 
 public class App extends Application {
-  private static final String WINGU_API_KEY = null; // TODO
+    private static final String WINGU_API_KEY = null; // TODO
 
-  @Override
-  public void onCreate() {
-    super.onCreate();
+    @Override
+    public void onCreate() {
+        super.onCreate();
 
-    final ChannelNotificationsConfig.ChannelNotificationCallback notificationCallback = collection -> {
-      if (collection.isEmpty()) {
-        return Collections.emptyList();
-      }
+        WinguSDKBuilder.with(this, WINGU_API_KEY)
+                .iBeaconBackgroundScan(true)
+                .channelNotificationsConfig(this::getChannelNotificationsConfig)
+//                .registerComponent(LocationWinguComponent.specMapView()) // TODO optional; needs google API key added to your AndroidManifest.xml
+//                .registerComponent(VideoWinguComponent.spec(your_google_api_key)) // TODO optional
+                .build();
+    }
 
-      final PendingIntent pendingIntent = PendingIntent.getActivity(App.this, 0, new Intent(App.this, MainActivity.class), 0);
-      final String title = collection.size() + " item(s) nearby";
-      final WinguNotification winguNotification = WinguNotification.createGroupNotification(collection, title, pendingIntent);
-      return Collections.singleton(winguNotification);
-    };
+    private ChannelNotificationsConfig getChannelNotificationsConfig() {
+        final ChannelNotificationsConfig.ChannelNotificationCallback notificationCallback = collection -> {
+            if (collection.isEmpty()) {
+                return Collections.emptyList();
+            }
 
-    WinguSDKBuilder.with(this, WINGU_API_KEY)
-        .iBeaconBackgroundScan(true)
-        .channelNotificationsConfig(() ->
-            new ChannelNotificationsConfig.Builder(notificationCallback)
+            final PendingIntent pendingIntent = PendingIntent.getActivity(App.this, 0, new Intent(App.this, MainActivity.class), 0);
+            final String title = collection.size() + " item(s) nearby";
+            final WinguNotification winguNotification = WinguNotification.createGroupNotification(collection, title, pendingIntent);
+            return Collections.singleton(winguNotification);
+        };
+
+        return new ChannelNotificationsConfig.Builder(notificationCallback)
                 .setSmallIcon(R.drawable.ic_notification_small)
                 .setLargeIcon(R.drawable.ic_notification_large)
-                .build())
-        .build();
-  }
+                .build();
+    }
 }
